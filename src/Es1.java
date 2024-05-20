7422c7da9b78a6aded5c90de5b8f9d1e0d15ed83
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-05-20 15:56:33 +0200

    Es 1 completed

---
+++ src/Es1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Es1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        System.out.println(sum(2,3));
+        System.out.println(numStrConcat("hello number ", 23));
+
+
+        String[] testArray = {"hello", "how", "are", "doing", "belandi"};
+        String testStr = "you";
+
+        System.out.println((Arrays.toString(insertInArray( testArray, testStr))));
+
+    }
+
+    public static int sum(int num1, int num2){
+        return num1+num2;
+    }
+
+    public static String numStrConcat(String value1,  int value2){
+        System.out.println(value1 + value2);
+        System.out.println(value1.concat(String.valueOf(value2)));
+        return "operation completed";
+    }
+
+    public static String[] insertInArray(String[] startingArr, String strToInsert){
+        String[] newArray = new String[6];
+        System.arraycopy(startingArr, 0, newArray, 0,3);
+        newArray[3] = strToInsert;
+        newArray[4] = startingArr[3];
+        newArray[5] = startingArr[4];
+
+        return newArray;
+
+    }
+}
---
+++ src/Es2.java
@@ -0,0 +1,9 @@
+public class Es2 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
